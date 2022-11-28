72494a7e8614f2f85fb5ad2580efb97d6b83f576
Kevin Romero Peces-Barba <hello@kevinrpb.me>
2022-11-28 10:41:08 +0100

    [sane-link] Fixes for Next 13

--- components/sane-link/src/sane-link.jsx
+++ components/sane-link/src/sane-link.jsx
@@ -3,18 +3,16 @@ import React from 'react'
 import Link from 'next/link'
 
 const SaneLink = (props) => {
-  const { children, onClick, href } = props
+  const { children, href, ...otherProps } = props
 
   // If the link is local it will start with a '/' or '#'
   // Otherwise it'll be something like 'https://'
   return href.startsWith('/') || href.startsWith('#') || href === '' ? (
-    <Link {...props}>
-      <a onClick={onClick} {...props}>
-        {children}
-      </a>
+    <Link href={href} {...otherProps}>
+      {children}
     </Link>
   ) : (
-    <a onClick={onClick} {...props} target='_blank' rel='noopener noreferrer'>
+    <a href={href} {...otherProps} target='_blank' rel='noopener noreferrer'>
       {children}
     </a>
   )
